cbd8fec47c6d15a87595ba6eb28fe32aaeb2e3f5
Pournami Puthenpurayil Rajan <pournami.puthenpurayilrajan@sjsu.edu>
2018-03-18 19:18:55 -0700

    L2 switch implementation with user input and output logs into a file

--- main.py
+++ main.py
@@ -1,30 +1,36 @@
 from switch import switch
 import sys
+import time
 
-# data_input2 --> Host to Mac address mapping
-data_input2 = {'A': '20-55-24-BD-4D-4C', 'B': '10-75-D3-79-22-CF', 'C': '5F-CA-A5-88-BC-38', 'D': '6B-26-7B-C4-51-C7'}
+text_file= open("write it.text","w")
+text_file.write("\n")
+text_file.write("SRC_MAC\t\t\t\t\t")
+text_file.write("DST_MAC\t\t\t\t\t")
+text_file.write("Port No\t\t")
+text_file.write("Action\t\n")
+text_file.close()
+s = switch()
 while True:
 
-    SRC_HOST = input("Enter the sender (A/B/C/D) :")
-    if SRC_HOST not in ("A", "B", "C", "D"):
-        print("{} - Not a valid source port number".format(SRC_HOST))
-        sys.exit()
 
-    DST_HOST = input("Enter the receiver (A/B/C/D) :")
-    if DST_HOST not in ("A", "B", "C", "D"):
-        print("{} - Not a valid destination port number".format(DST_HOST))
-        sys.exit()
-
-    if DST_HOST == SRC_HOST:
+    text_file = open("write it.text", "a")
+    text_file.write("\n")
+    text_file.close()
+    SRC_MAC = input("Enter the Source MAC Address :")
+    DST_MAC = input("Enter the Destination MAC Address :")
+    time1 = time.time()
+    print("time1:", time1)
+    if DST_MAC == SRC_MAC:
         print("Destination and source cannot be same. Try Again !! ")
         sys.exit()
-
-    SRC_MAC = data_input2[SRC_HOST]
-    DST_MAC = data_input2[DST_HOST]
-
-    # print("SRC MAC : {} \n DST MAC : {}".format(SRC_MAC,DST_MAC))
-
-    s = switch()
     packet = [DST_MAC, SRC_MAC]
     s.validate(packet)
     s.forward(packet)
+    time2=time.time()
+    print("time2:", time2)
+    print(s.FWD_TABLE)
+    print("Total time for forwarding ", (time2-time1)*1000, "ms")
+
+
+
+
--- switch.py
+++ switch.py
@@ -1,10 +1,14 @@
 import sys
 class switch:
-    FWD_TABLE = {}
-    # data_input1 --> Mac address to port mapping
-    data_input1 = {'20-55-24-BD-4D-4C': 1, '10-75-D3-79-22-CF': 2, '5F-CA-A5-88-BC-38': 3, '6B-26-7B-C4-51-C7': 4}
 
-    # FWD_TABLE={}
+    FWD_TABLE = {}
+    data_input1={}
+    ports = ['1','2','3','4']
+    file = open("mac_to_port.txt", "r")
+    for line in file:
+        a = line.split("port")
+        data_input1[a[0].strip()]=a[1][0]
+    file.close()
 
     def learn(self, SRC_MAC):
         if not SRC_MAC in self.FWD_TABLE:
@@ -16,52 +20,78 @@ class switch:
         self.learn(SRC_MAC)
         if DST_MAC in self.FWD_TABLE:
             port = self.FWD_TABLE[DST_MAC]
+            input_port = self.data_input1[SRC_MAC]
+            if port == input_port:
+                text_file = open("write it.text", "a")
+                text_file.write("\n")
+                text_file.write(packet[1])
+                text_file.write("\t\t")
+                text_file.write(packet[0])
+                text_file.write("\t\t\t\t\t ")
+                text_file.write(" Suppressed : Same port number")
+                text_file.close()
+                return
             self.sendpacket(port, packet)
         else:
+            port = self.data_input1[DST_MAC]
             input_port = self.data_input1[SRC_MAC]
+            if port == input_port:
+                text_file = open("write it.text", "a")
+                text_file.write("\n")
+                text_file.write(packet[1])
+                text_file.write("\t\t")
+                text_file.write(packet[0])
+                text_file.write("\t\t\t\t\t")
+                text_file.write("Suppressed : Same port number")
+                text_file.close()
+                return
             self.broadcast(input_port, packet)
 
     def sendpacket(self, port, packet):
         DST_MAC = packet[0]
+        #print("inside fn")
         if port == self.data_input1[DST_MAC]:
             Action = "Accepted"
+            #print("acc")
         else:
-            Action = "Rejected"
-        print("Packet received")
-        print("Details of the packet")
-        print("----------------------")
-        print("SRC_MAC : ", packet[1])
-        print("DST_MAC : ", packet[0])
-        print("Port No : ", port)
-        print("Action  : ", Action)
+            Action = "Rejected(Broadcast packet)"
+            #print("rej")
+        text_file = open("write it.text", "a")
+        text_file.write("\n")
+        text_file.write(packet[1])
+        text_file.write("\t\t")
+        text_file.write(packet[0])
+        text_file.write("\t\t")
+        text_file.write(port)
+        text_file.write("\t\t\t")
+        text_file.write(Action)
+        text_file.close()
+
 
     def broadcast(self, input_port, packet):
-        for key in self.data_input1:
-            current_port = self.data_input1[key]
-            if current_port != input_port:
-                self.sendpacket(current_port, packet)
-
-    def check_(self, mac_int):
-        if mac_int <= 255:
-            return "The MAC address has the proper format"
-        else:
-            return "the MAC address does not have the proper format"
+        for p in self.ports:
+            if p != input_port:
+                self.sendpacket(p, packet)
 
 
-    def validate(self,packet):
-        a = []
-        for i in packet:
-            if not (len(i) == 17):
-                print("{} - does not have the proper length".format(i))
-            else:
-                print("{} - has the proper length".format(i))
-            a = i.split("-")
-            for f in a:
-                try:
-                    p = int(f, 16)
-                    k = self.check_(p)
-                except ValueError:
-                    k = "Not a hex number. Improper MAC address format"
-                    sys.exit()
-            print(k)
-
+    def validate(self, packet):
+        SRC_MAC = packet[1]
+        DST_MAC = packet[0]
+        if SRC_MAC not in self.data_input1:
+            text_file = open("write it.text", "a")
+            text_file.write("\n")
+            text_file.write(SRC_MAC)
+            text_file.write("\t\t\t\t\t")
+            text_file.write("\t\t\t\t\t\t")
+            text_file.write("Invalid Source MAC entry found")
+            text_file.close()
+            sys.exit()
+        if DST_MAC not in self.data_input1:
+            text_file = open("write it.text", "a")
+            text_file.write("\n")
+            text_file.write("\t\t\t\t\t\t")
+            text_file.write(DST_MAC)
+            text_file.write("\t\t\t\t\t")
+            text_file.write("Invalid Destination MAC entry found")
+            text_file.close()
+            sys.exit()
