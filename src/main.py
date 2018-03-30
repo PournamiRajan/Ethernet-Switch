0a7e42f006d3779007e88f24dd4a949eea0c10d0
Pournami Puthenpurayil Rajan <pournami.puthenpurayilrajan@sjsu.edu>
2018-03-30 00:10:19 -0700

    stage 3: Added VLAN to L2 switch

--- src/main.py
+++ src/main.py
@@ -1,14 +1,20 @@
 from switch import switch
-import sys
 import time
 
 text_file= open("write it.text","w")
 text_file.write("\n")
 text_file.write("SRC_MAC\t\t\t\t\t")
 text_file.write("DST_MAC\t\t\t\t\t")
-text_file.write("Port No\t\t")
+text_file.write("\t\t\t  ")
 text_file.write("Action\t\n")
 text_file.close()
+
+perf_file= open("performance_summary.text", "w")
+perf_file.write("\n")
+perf_file.write("# of entries in FWD TABLE\t\t\t")
+perf_file.write("Time taken\t\t\t\t\t\n")
+perf_file.close()
+
 s = switch()
 while True:
 
@@ -16,20 +22,24 @@ while True:
     text_file = open("write it.text", "a")
     text_file.write("\n")
     text_file.close()
-    SRC_MAC = input("Enter the Source MAC Address :")
-    DST_MAC = input("Enter the Destination MAC Address :")
+    in_port = input("Enter input port :").strip()
+    SRC_MAC = input("Enter the Source MAC Address :").strip()
+    DST_MAC = input("Enter the Destination MAC Address :").strip()
+    vlan_id = input("Enter the VLAN ID of incoming packet :").strip()
     time1 = time.time()
-    print("time1:", time1)
-    if DST_MAC == SRC_MAC:
-        print("Destination and source cannot be same. Try Again !! ")
-        sys.exit()
-    packet = [DST_MAC, SRC_MAC]
-    s.validate(packet)
-    s.forward(packet)
-    time2=time.time()
-    print("time2:", time2)
-    print(s.FWD_TABLE)
-    print("Total time for forwarding ", (time2-time1)*1000, "ms")
+    packet = [DST_MAC, SRC_MAC, vlan_id]
+    s.validate(packet, in_port)
+    s.forward(packet, in_port)
+    time2 = time.time()
+    perf_file= open("performance_summary.text", "a")
+    perf_file.write("\t\t")
+    perf_file.write(str(len(s.FWD_TABLE)))
+    perf_file.write("\t\t\t\t\t\t")
+    perf_file.write(str((time2-time1)*1000))
+    perf_file.write("\n")
+    perf_file.close()
+
+    #print(s.FWD_TABLE)
 
 
 
--- src/switch.py
+++ src/switch.py
@@ -2,96 +2,108 @@ import sys
 class switch:
 
     FWD_TABLE = {}
-    data_input1={}
-    ports = ['1','2','3','4']
+    vlan_to_ports = {'100': ['0', '1', '2'], '200': ['3', '4', '5', '6'], '300': ['7', '8', '9']}
+
     file = open("mac_to_port.txt", "r")
     for line in file:
         a = line.split("port")
-        data_input1[a[0].strip()]=a[1][0]
+        vlanid = a[1][8:11]
+        key = vlanid+a[0].strip()
+        FWD_TABLE[key] = a[1][0]
     file.close()
 
-    def learn(self, SRC_MAC):
-        if not SRC_MAC in self.FWD_TABLE:
-            self.FWD_TABLE[SRC_MAC] = self.data_input1[SRC_MAC]
-
-    def forward(self, packet):
+    def learn(self, packet, in_port):
         SRC_MAC = packet[1]
-        DST_MAC = packet[0]
-        self.learn(SRC_MAC)
-        if DST_MAC in self.FWD_TABLE:
-            port = self.FWD_TABLE[DST_MAC]
-            input_port = self.data_input1[SRC_MAC]
-            if port == input_port:
-                text_file = open("write it.text", "a")
-                text_file.write("\n")
-                text_file.write(packet[1])
-                text_file.write("\t\t")
-                text_file.write(packet[0])
-                text_file.write("\t\t\t\t\t ")
-                text_file.write(" Suppressed : Same port number")
-                text_file.close()
-                return
-            self.sendpacket(port, packet)
-        else:
-            port = self.data_input1[DST_MAC]
-            input_port = self.data_input1[SRC_MAC]
-            if port == input_port:
-                text_file = open("write it.text", "a")
-                text_file.write("\n")
-                text_file.write(packet[1])
-                text_file.write("\t\t")
-                text_file.write(packet[0])
-                text_file.write("\t\t\t\t\t")
-                text_file.write("Suppressed : Same port number")
-                text_file.close()
-                return
-            self.broadcast(input_port, packet)
+        vlan_id = packet[2]
+        key = vlan_id + SRC_MAC
+        if key not in self.FWD_TABLE:
+            self.add_to_FWD_TABLE(key, str(in_port))
 
-    def sendpacket(self, port, packet):
+
+    def forward(self, packet, in_port):
         DST_MAC = packet[0]
-        #print("inside fn")
-        if port == self.data_input1[DST_MAC]:
-            Action = "Accepted"
-            #print("acc")
+        vlan_id = packet[2]
+        self.learn(packet, in_port)
+        key = vlan_id + DST_MAC
+        if self.if_present_in_FWD_TABLE(key):
+            out_port = self.get_port_from_FWD_TABLE(key)
+            self.sendpacket(out_port, in_port, packet)
         else:
-            Action = "Rejected(Broadcast packet)"
-            #print("rej")
+            self.broadcast(in_port, packet)
+
+    def sendpacket(self, out_port, in_port, packet):
         text_file = open("write it.text", "a")
         text_file.write("\n")
         text_file.write(packet[1])
         text_file.write("\t\t")
         text_file.write(packet[0])
-        text_file.write("\t\t")
-        text_file.write(port)
-        text_file.write("\t\t\t")
-        text_file.write(Action)
+        text_file.write("\t\t\t\t\t")
+        if str(out_port) == str(in_port):
+            text_file.write("Packet sent on port : " + str(out_port) + "(Suppress)")
+        else:
+            text_file.write("Packet sent on port : " + str(out_port))
         text_file.close()
 
+    def broadcast(self, in_port, packet):
+        vlan = packet[2]
+        for port in self.vlan_to_ports[vlan]:
+            self.sendpacket(port, in_port, packet)
 
-    def broadcast(self, input_port, packet):
-        for p in self.ports:
-            if p != input_port:
-                self.sendpacket(p, packet)
-
-
-    def validate(self, packet):
+    def validate(self, packet, in_port):
         SRC_MAC = packet[1]
         DST_MAC = packet[0]
-        if SRC_MAC not in self.data_input1:
-            text_file = open("write it.text", "a")
-            text_file.write("\n")
-            text_file.write(SRC_MAC)
-            text_file.write("\t\t\t\t\t")
-            text_file.write("\t\t\t\t\t\t")
-            text_file.write("Invalid Source MAC entry found")
-            text_file.close()
+        vlan_id = packet[2]
+        if not self.validate_mac_format(SRC_MAC):
+            print("Invalid entry : " + SRC_MAC + " not in proper format")
             sys.exit()
-        if DST_MAC not in self.data_input1:
-            text_file = open("write it.text", "a")
-            text_file.write("\n")
-            text_file.write("\t\t\t\t\t\t")
-            text_file.write(DST_MAC)
-            text_file.write("\t\t\t\t\t")
-            text_file.write("Invalid Destination MAC entry found")
-            text_file.close()
+        if not self.validate_mac_format(DST_MAC):
+            print("Invalid entry : " + DST_MAC + " not in proper format")
             sys.exit()
+        if DST_MAC == SRC_MAC:
+            print("Invalid entry : Destination and source cannot be same. Try Again !! ")
+            sys.exit()
+        if vlan_id not in self.vlan_to_ports:
+            print("Invalid entry : VLAN_ID not found/configured")
+            sys.exit()
+        if in_port not in self.vlan_to_ports[vlan_id]:
+            print("Invalid entry : VLAN source mapping not found")
+            sys.exit()
+
+    def validate_mac_format(self, mac):
+        valid = True
+        if not (len(mac) == 17):
+            valid = False
+        byte_list = mac.split(":")
+        for each_byte in byte_list:
+            try:
+                mac_int = int(each_byte, 16)
+                if not mac_int <= 255:
+                    valid = False
+            except ValueError:
+                valid = False
+        return valid
+
+    def check_(self, mac_int):
+        if mac_int <= 255:
+            return "The MAC address has the proper format"
+        else:
+            return "the MAC address does not have the proper format"
+
+    def add_to_FWD_TABLE(self, key, in_port):
+        self.FWD_TABLE[key] = in_port
+
+    def if_present_in_FWD_TABLE(self, key):
+        if key in self.FWD_TABLE:
+            return True
+        else:
+            return False
+
+    def get_port_from_FWD_TABLE(self, key):
+        port = self.FWD_TABLE[key]
+        return port
+
+
+
+
+
+
